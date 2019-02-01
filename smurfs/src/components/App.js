e06402cdd95d56d3e0da00a2f4e6dbc8f49b739e
Angel Torres <alas4224@gmail.com>
2019-02-01 11:16:14 -0600

    was able to fetch smurfs from database and display them on page

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions'
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +9,30 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getSmurfs()
+  }
+
   render() {
+    console.log(this.props)
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>SMURFS DATABASE</h1>
+          {this.props.smurfs.map( smurf => (
+            <div>
+              <h3>{smurf.name}</h3>
+              <p>{smurf.age}</p>
+              <p>{smurf.height}</p>
+            </div>
+          ))}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  smurfs: state.smurfs
+})
+
+export default connect(mapStateToProps, { getSmurfs })(App);
